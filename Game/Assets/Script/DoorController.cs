838cb74152fd2adc7763ec660da55abc5c9a50f8
FALL Simply <simply@fallproject.org>
2019-05-13 12:55:58 -0500

    Initial commit

---
+++ Game/Assets/Script/DoorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+	// Start is called before the first frame update
+
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	void OnCollisionEnter(Collision hit) {
+		// if (hit.gameObject.CompareTag("Door")) {
+		this.gameObject.transform.Rotate(0, -90f, 0);
+		Debug.Log(ColliderHit);
+		// }
+	}
+}

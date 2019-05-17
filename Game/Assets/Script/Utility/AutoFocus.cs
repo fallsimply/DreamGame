40e03fa15cc6a142f9a2d0602b11537965f3041b
FALL Simply <simply@fallproject.org>
2019-05-17 15:00:18 -0500

    Daily Commit 5/17/2019

---
+++ Game/Assets/Script/Utility/AutoFocus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class AutoFocus : MonoBehaviour {
+	// Start is called before the first frame update
+	void Start() {
+		EventSystem eventSys = EventSystem.current;
+		eventSys.SetSelectedGameObject(this.gameObject);
+	}
+}
